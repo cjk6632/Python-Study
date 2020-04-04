ae65b98fccb6230eec62cff53c8478a74b4d8a4b
cjk6632 <cjk6632@naver.com>
2020-04-04 16:12:50 +0900

    함수(def) 및 file 읽고 쓰기

---
+++ P-Week3/Q/Q6.py
@@ -0,0 +1,11 @@
+# 사용자의 입력을 파일(test.txt)에 저장하는 프로그램을 작성
+# (단 프로그램을 다시 실행하더라도 기존에 작성한 내용을 유지하고 새로 입력한 내용을 추가. = 'a' 모드 사용 (append))
+
+
+user_input = input("저장할 내용을 입력 : ")
+f = open('test.txt', 'a') # with open('test.txt', 'a') as f: 와 같음
+f.write(user_input)
+f.write("\n")
+f.close()
+
+
